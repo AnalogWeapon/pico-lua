ef8a25428bb9cb580bf24dd09eb0254555544874
Nic Stage <nic.stage@gmail.com>
2025-04-21 15:37:36 -0500

    for windows with git bash

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ cd ./test_program
 mkdir build
 rm -rf build/*
 cd build
-cmake .. -DPICO_SDK_PATH=$PICO_SDK_PATH -DPICO_BOARD=pico2
-make -j4
+cmake .. -DPICO_SDK_PATH=$PICO_SDK_PATH -DPICO_BOARD=pico2 -G Ninja -B .
+cmake --build .
